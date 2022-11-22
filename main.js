1e93530dbfad5605ba7df6d72b8bf9bc225eb23b
User <paulate.alonso@gmail.com>
2022-11-22 21:20:49 +0100

    WithoutCounter

--- main.js
+++ main.js
@@ -1,61 +1,93 @@
-const buttonBag = document.querySelector("#bagLogo")
 const btnAdd1 = document.querySelector("#btnAdd1")
 const btnAdd2 = document.querySelector("#btnAdd2")
 const btnAdd3 = document.querySelector("#btnAdd3")
 const btnAdd4 = document.querySelector("#btnAdd4")
 const shopCart = document.querySelector("#shopCart")
-let divisa = "€";
 
-const productStr = [{
-    name: "Costa Rica Tarrazú",
-    price: 9.00,
-},
-{
-    name: "Colombia Los Naranjos",
-    price: 9.00,
-},
-{
-    name: "Los Amanecer",
-    price: 9.00,
-},
-{
-    name: "Etiopía Yrgacheff",
-    price: 9.00,
-}
+let productStorage = []
+
+localStorage.clear()
+
+productStorage = localStorage.getItem('productStr') ? JSON.parse(localStorage.getItem('productStr')) : []
+
+
+product = [
+    {
+        idProduct: 1,
+        name: "Costa Rica Tarrazú",
+        price: 9.00,
+        quantity: 0
+    },
+    {
+        idProduct: 2,
+        name: "Colombia Los Naranjos",
+        price: 9.00,
+        quantity: 0
+    },
+    {
+        idProduct: 3,
+        name: "Los Amanecer",
+        price: 9.00,
+        quantity: 0
+    },
+    {
+        idProduct: 4,
+        name: "Etiopía Yrgacheff",
+        price: 9.00,
+        quantity: 0
+    }
 ]
-
-let productStorage = localStorage.getItem('productStr') ? JSON.parse(localStorage.getItem('productStr')) : []
-
 
 const selectProduct = (button) => {
 
     const coffe = document.createElement("li")
-    const price = 9.00;
+    shopCart.appendChild(coffe)
 
     if (button == "btnAdd1") {
-        console.log("PIRAÑA1");
-        coffe.innerHTML = `<h5>Costa Rica Tarrazú</h5> ${price}`
-        productStorage.push(productStr[0])
-        localStorage.setItem('productStr'), JSON.stringify(productStorage)
+        if (productStorage.includes(product[0])) {
+            product[0].quantity++
+            coffe.innerHTML = `${product[0].quantity}`
+            productStorage.push(product[0])
+            localStorage.setItem('productStr', JSON.stringify(productStorage))
+        } else {
+            product[0].quantity++
+
+            coffe.innerHTML = `<h6>${product[0].name}</h6> ${product[0].price}€ <button>+</button>${product[0].quantity}<button>-</button> `
+            productStorage.push(product[0])
+            localStorage.setItem('productStr', JSON.stringify(productStorage))
+        }
     }
     if (button == "btnAdd2") {
-        console.log("PIRAÑA2");
-        coffe.innerHTML = `<h5>Colombia Los Naranjos</h5> ${price}`
-        productStorage.push(productStr[1])
-        localStorage.setItem('productStr'), JSON.stringify(productStorage)
+        product[1].quantity++
+        coffe.innerHTML = `<h6>${product[1].name}</h6> ${product[1].price}€ <button>+</button>${product[1].quantity}<button>-</button> `
+        productStorage.push(product[1])
+        localStorage.setItem('productStr', JSON.stringify(productStorage))
     }
     if (button == "btnAdd3") {
-        console.log("PIRAÑA3");
-        coffe.innerHTML = `<h5>Los Amanecer</h5> ${price}`
-        productStorage.push(productStr[2])
-        localStorage.setItem('productStr'), JSON.stringify(productStorage)
+        product[2].quantity++
+        coffe.innerHTML = `<h6>${product[2].name}</h6> ${product[2].price}€ <button>+</button>${product[2].quantity}<button>-</button> `
+        productStorage.push(product[2])
+        localStorage.setItem('productStr', JSON.stringify(productStorage))
     }
     if (button == "btnAdd4") {
-        console.log("PIRAÑA4");
-        coffe.innerHTML = `<h5>Etiopía Yrgacheff</h5> ${price}`
-        productStorage.push(productStr[3])
-        localStorage.setItem('productStr'), JSON.stringify(productStorage)
+        product[3].quantity++
+        coffe.innerHTML = `<h6>${product[3].name}</h6> ${product[3].price}€ <button>+</button>${product[3].quantity}<button>-</button> `
+        productStorage.push(product[3])
+        localStorage.setItem('productStr', JSON.stringify(productStorage))
     }
-    shopCart.appendChild(coffe)
-    console.log(coffe, shop);
 }
+
+
+const buttonBag = document.querySelector("#bagLogo")
+
+buttonBag.onclick = (e) => {
+    e.preventDefault()
+
+    if (shopCart.style.display === "none") {
+        shopCart.style.display = "block"
+    } else {
+        shopCart.style.display = "none"
+    }
+}
+
+
